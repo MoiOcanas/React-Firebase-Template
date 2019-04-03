b3b120cb023ba476f2d1d0a5245b45f193e96233
MoiOcanas <alejandro.ocanas@outlook.com>
2019-04-03 15:36:07 -0500

    Creating Results styles file

---
+++ src/components/Results.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+//Styles
+import './styles/results.css';
+
+class Results extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+
+        };
+    }
+    render() {
+        return(
+            <div></div>
+        );
+    }
+}
+
+export default Results;
