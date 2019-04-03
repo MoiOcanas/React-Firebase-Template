c9bc4a3c17c4a8ddd22bac9517936006f853110c
MoiOcanas <alejandro.ocanas@outlook.com>
2019-04-03 12:14:33 -0500

    Styling the Request component

--- src/components/Request.js
+++ src/components/Request.js
@@ -6,28 +6,28 @@ import './styles/request.css';
 class Request extends Component {
     render() {
         return (
-            <div>
-                <form onSubmit={this.onSubmit}>
+            <div className="main-container-request">
+                <form onSubmit={this.onSubmit} className="form-container-request">
                     <input
                         name="username"
-                        className="input-form-signup"
+                        className="input-form-request"
                         onChange={this.onChange}
                         type="text"
                         placeholder="Full Name"
                     />
                     <input
                         name="email"
-                        className="input-form-signup"
+                        className="input-form-request"
                         type="text"
                         placeholder="Email Address"
                     />
                     <input
                         name="passwordOne"
-                        className="input-form-signup"
+                        className="input-form-request"
                         type="password"
                         placeholder="Password"
                     />
-                    <button type="submit" className="input-button-signup">Sign Up</button>
+                    <button type="submit" className="input-button-request">Sign Up</button>
                 </form>
             </div>
         );
